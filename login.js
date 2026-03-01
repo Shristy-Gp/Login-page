f62100120479af3121522d00830ba10a78c3ab12
Shristy-Gp <shristygupta275@gmail.com>
2026-03-01 17:56:03 +0530

    created login js

---
+++ login.js
@@ -0,0 +1,5 @@
+function show(Class){
+    document.querySelector(".content").classList.add("hidden");
+    document.querySelector(".content1").classList.add("hidden");
+    document.querySelector("." + Class).classList.remove("hidden");
+}
